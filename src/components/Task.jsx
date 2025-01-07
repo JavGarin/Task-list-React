3f116feaa0edb815eb1c9aca75f53027997c23be
JavGarin <hlm.javiergarin@gmail.com>
2025-01-07 19:25:41 -0300

    add files Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
+import '../Task.css';
 
+const Task = ({ tarea, id, deleteTask }) => {
+    const [completada, setCompletada] = useState(false);
 
-const Task = () => {
     return (
-    <div>
-        
+    <div
+        className={
+        completada
+            ? 'containerTarea containerTareaCompletada'
+            : 'containerTarea'
+        }
+    >
+        <h2 className={completada ? 'completada' : 'noCompletada'}>{tarea}</h2>
+        <button
+        id="completar"
+        onClick={() => setCompletada(!completada)}
+        >
+        {completada ? 'No completada' : 'Completada'}
+        </button>
+        <button onClick={() => deleteTask(id)} id="eliminar">
+        Eliminar
+        </button>
     </div>
-    )
-}
+    );
+};
 
-export {Task}
+export { Task };
