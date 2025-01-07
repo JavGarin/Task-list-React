b7633f99b16e7ca20d59ab4722ba17b6238c9aa0
JavGarin <hlm.javiergarin@gmail.com>
2025-01-07 16:10:46 -0300

    add files Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,14 @@
-import React from "react";
 
-const Form = () => {
+
+const Form = (handleChange, addTask, tarea) => {
     return (
     <div>
-
+        <form onSubmit={addTask}>
+        <input type="text" value={tarea} placeholder="Ingresa la tarea" onChange={handleChange} />
+        <button type="submit">Agregar</button>
+        </form>
     </div>
     )
 }
 
-export default Form;
+export {Form}
