777d24648fde3a70712e45b3a83aad7bf7c8317e
JavGarin <hlm.javiergarin@gmail.com>
2025-01-07 19:24:23 -0300

    add files App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,52 @@
-import { useState } from 'react'
-import './App.css'
-import { Task } from './components/Task'
-import { Form } from './components/Form'
+import { useState } from 'react';
+import './App.css';
+import { Task } from './components/Task';
+import { Form } from './components/Form';
 
 function App() {
-  const [tarea, setTarea] = useState('')
-  const [tareas, setTareas] = useState([])
+  const [tarea, setTarea] = useState('');
+  const [tareas, setTareas] = useState([]);
 
   const handleChange = (e) => {
-    setTarea(e.target.value)
-  }
+    setTarea(e.target.value);
+  };
 
   const addTask = (e) => {
     e.preventDefault();
-    if(tarea.trim() === '') {
-      console.log('Debes agregar una tarea')
+    if (tarea.trim() === '') {
+      console.log('Debes agregar una tarea');
       return;
     }
 
     const nuevaTarea = {
       id: Date.now(),
       tarea,
-      completada: false
-    }
+      completada: false,
+    };
 
-    const totalTareas = [nuevaTarea,...tareas]
-    setTareas(totalTareas)
-    setTarea('')
-  }
+    setTareas([nuevaTarea, ...tareas]);
+    setTarea('');
+  };
+
+  const deleteTask = (id) => {
+    const tareasActualizadas = tareas.filter((t) => t.id !== id);
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <>
-      <h2>Todo list</h2>
-      <Form
-        handleChange={handleChange}
-        addTask={addTask}
-        tarea={tarea}
+      <h2>Task List React</h2>
+      <Form handleChange={handleChange} addTask={addTask} tarea={tarea} />
+      {tareas.map((t) => (
+        <Task
+          key={t.id}
+          id={t.id}
+          tarea={t.tarea}
+          deleteTask={deleteTask}
         />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
