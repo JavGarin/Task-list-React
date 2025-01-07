f3a560680c2b256d6dba8c64b6cc70c774478945
JavGarin <hlm.javiergarin@gmail.com>
2025-01-07 19:27:07 -0300

    add files Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,17 @@
-
-
-const Form = (handleChange, addTask, tarea) => {
+const Form = ({ handleChange, addTask, tarea }) => {
     return (
-    <div>
+        <div>
         <form onSubmit={addTask}>
-        <input type="text" value={tarea} placeholder="Ingresa la tarea" onChange={handleChange} />
-        <button type="submit">Agregar</button>
+            <input
+            type="text"
+            value={tarea}
+            placeholder="Ingresa la tarea"
+            onChange={handleChange}
+            />
+            <button type="submit">Agregar</button>
         </form>
-    </div>
-    )
-}
+        </div>
+    );
+    };
 
-export {Form}
+    export { Form };
