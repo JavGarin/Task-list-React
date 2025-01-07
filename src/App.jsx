343a48ddb2d006bf8a01a1e74de4e8bbb5093edb
JavGarin <hlm.javiergarin@gmail.com>
2025-01-07 16:09:04 -0300

    add files App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Task } from './components/Task'
+import { Form } from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tarea, setTarea] = useState('')
+  const [tareas, setTareas] = useState([])
+
+  const handleChange = (e) => {
+    setTarea(e.target.value)
+  }
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if(tarea.trim() === '') {
+      console.log('Debes agregar una tarea')
+      return;
+    }
+
+    const nuevaTarea = {
+      id: Date.now(),
+      tarea,
+      completada: false
+    }
+
+    const totalTareas = [nuevaTarea,...tareas]
+    setTareas(totalTareas)
+    setTarea('')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Todo list</h2>
+      <Form
+        handleChange={handleChange}
+        addTask={addTask}
+        tarea={tarea}
+        />
     </>
   )
 }
